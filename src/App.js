6f12bf5ea91fa543093ffe0c1917a9b6b58c45b1
Fauzer <fauzersantos@gmail.com>
2024-06-12 22:07:26 -0300

    projeto-rotas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+import "./App.css";
+import Home from "./components/Home";
+import Mapas from "./components/Mapas";
+import Temperatura from "./components/Temperatura";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Rotas</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Home />} path="/" />
+          <Route element={<Mapas />} path="/mapas" />
+          <Route element={<Temperatura />} path="/temperatura" />
+        </Routes>
+      </BrowserRouter>
+      {/*<Home></Home>
+      <Mapas></Mapas>
+      <Temperatura></Temperatura>*/}
     </div>
   );
 }
